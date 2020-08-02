5fbc68d0a52bf2ba16cd19ae5dce705a4d1717df
Karthikeyan <skarthikeyan100@gmail.com>
2020-08-02 22:49:36 +0530

    Initial school site

---
+++ precache-manifest.6f0ac329c4549b36c5c028712ce56afc.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad482c9ed56da9b335434187a86a8a9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cfd7a23793f07bcab1bf",
+    "url": "./static/css/main.3dcb44b9.chunk.css"
+  },
+  {
+    "revision": "cee7ba23c1a73486ff44",
+    "url": "./static/js/2.8b879f3f.chunk.js"
+  },
+  {
+    "revision": "8f921916a9d556de5fe4601a7984f05c",
+    "url": "./static/js/2.8b879f3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cfd7a23793f07bcab1bf",
+    "url": "./static/js/main.6463e643.chunk.js"
+  },
+  {
+    "revision": "f93057799f675317d918",
+    "url": "./static/js/runtime-main.a7756202.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "./static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "./static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "./static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "./static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "./static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "./static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "./static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "./static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "11f6cfcb0c32e687421b04eecdc15c63",
+    "url": "./static/media/slide1.11f6cfcb.jpg"
+  },
+  {
+    "revision": "c529a35388968921febd6a82067142cc",
+    "url": "./static/media/slide2.c529a353.jpg"
+  }
+]);
